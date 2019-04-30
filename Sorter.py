b667e28713c0d4ba4d20393a844536a16307d754
rtperezyera <A01374866@itesm.mx>
2019-04-30 08:22:41 -0500

    Completed mergeSort() but its not working.

--- Sorter.py
+++ Sorter.py
@@ -39,14 +39,13 @@ def mergeSort(list):
             list[0], list[1] = list[1], list[0]
             return list
         else:
-            # simple swap
             return list
     else:
         merge( mergeSort(list[0:len(list)//2]), mergeSort(list[len(list)//2:len(list)]) )
 
 def merge(lst1, lst2):
-    print(lst1, "ton")
-    print(lst2, "ta")
+    print(lst1, "manz")
+    print(lst2, "ana")
     merged = [0]*(len(lst1) + len(lst2))
 
     print(len(merged))
@@ -57,8 +56,25 @@ def merge(lst1, lst2):
     k = 0
 
     while i < len(lst1) and j < len(lst2):
-        # do shit
-        pass
+        if lst1[i] > lst2[j]:
+            merged[k] = lst2[j]
+            k += 1
+            j += 1
+        else:
+            merged[k] = lst1[i]
+            k += 1
+            i += 1
+    # exit while bc condition is no longer satisfied
+    # one of the input arrays could still contain elems tho
+    # traverse that array and insert them
+    for m in range(i, len(lst1), 1):
+        merged[k] = lst1[m]
+        k += 1
+    for m in range(j, len(lst2), 1):
+        merged[k] = lst2[m]
+        k += 1
+
+    return merged
 
 
 print(mergeSort(laLista))
